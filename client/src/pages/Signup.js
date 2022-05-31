727bc2d966fe77b28bbf1fff254ded4e049558b6
wfk-tokunaga <wktokunaga@gmail.com>
2022-05-31 11:20:59 -0700

    finished user page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+  // Create a js function that wraps around our mutation code
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -14,8 +19,21 @@ const Signup = () => {
   };
 
   // submit form
-  const handleFormSubmit = async (event) => {
+  // submit form (notice the async!)
+  const handleFormSubmit = async event => {
     event.preventDefault();
+
+    // use try/catch instead of promises to handle errors
+    try {
+      const { data } = await addUser({
+        variables: { ...formState }
+      });
+    
+      // set token to localStorage and reload page to homepage
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
@@ -56,6 +74,7 @@ const Signup = () => {
                 Submit
               </button>
             </form>
+            {error && <div>Sign up failed</div>}
           </div>
         </div>
       </div>
