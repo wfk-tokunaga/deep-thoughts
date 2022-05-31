cebc58ea7a84729243629ad4ae3fa9753ab2c621
wfk-tokunaga <wktokunaga@gmail.com>
2022-05-30 19:27:25 -0700

    fixed route bug

--- client/src/App.js
+++ client/src/App.js
@@ -47,7 +47,7 @@ function App() {
                 element={<Signup />} 
               />
               <Route 
-                path="/profile" 
+                path="/profile/:username" 
                 element={<Profile />} 
               />
               <Route 
